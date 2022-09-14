931c566d8c362d2dda4c3700a8f020710612a058
stevenht19 <mstvdev19@gmail.com>
2022-09-14 09:11:53 -0500

    initial commit

---
+++ src/main/java/pe/isil/moduloseguridad/ModuloSeguridadApplication.java
@@ -0,0 +1,11 @@
+package pe.isil.moduloseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloSeguridadApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloSeguridadApplication.class, args);
+	}
+}
